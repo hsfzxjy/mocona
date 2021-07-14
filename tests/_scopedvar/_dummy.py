bdc00484f68102fc16b52438dd7cbfb45a77c239
hsfzxjy <hsfzxjy@gmail.com>
2021-07-14 20:29:19 +0800

    Add tests for Var proxy

---
+++ tests/_scopedvar/_dummy.py
@@ -0,0 +1,79 @@
+import operator
+
+
+class _Dummy:
+    def __init__(self, value):
+        self.value = value if isinstance(value, tuple) else (value, )
+        self.immutable = True
+
+    def _build_value(self, *args):
+        lst = list(self.value)
+        for arg in args:
+            if isinstance(arg, _Dummy):
+                lst.extend(arg.value)
+            else:
+                lst.append(arg)
+
+        return tuple(lst)
+
+    def _make_func(name):
+        def _func(self, *args):
+            return _Dummy(self._build_value(name, *args))
+
+        return _func
+
+    for funcname in ("__add__", "__sub__", "__mul__", "__mod__", "__divmod__",
+                     "__pow__", "__neg__", "__pos__", "__abs__", "__invert__",
+                     "__lshift__", "__rshift__", "__and__", "__xor__", "__or__",
+                     "__floordiv__", "__truediv__", "__matmul__",
+                     "__contains__", "__getitem__", "__setitem__",
+                     "__delitem__", "__reversed__", "__round__"):
+        locals()[funcname] = _make_func(funcname)
+
+    def _make_func(name):
+        def _func(self, *args):
+            value = self._build_value(name, *args)
+            if self.immutable:
+                self = _Dummy(value)
+                self.immutable = True
+            else:
+                self.value = value
+            return self
+
+        return _func
+
+    for funcname in ("__iadd__", "__isub__", "__imul__", "__imod__", "__ipow__",
+                     "__ilshift__", "__irshift__", "__iand__", "__ixor__",
+                     "__ior__", "__ifloordiv__", "__itruediv__", "__imatmul__"):
+        locals()[funcname] = _make_func(funcname)
+
+    def _make_func(name):
+        def _func(self, other):
+            if not isinstance(other, _Dummy): return False
+            return getattr(operator, name)(self.value, other.value)
+
+        return _func
+
+    for opname in ("ne", "eq", "le", "lt", "ge", "gt"):
+        locals()[f"__{opname}__"] = _make_func(opname)
+
+    del _make_func
+
+    def __int__(self):
+        return self.value[0]
+
+    __float__ = __bool__ = __bytes__ = __index__ = __str__ = __complex__ = __int__
+
+    def __len__(self):
+        return len(self.value)
+
+    def __enter__(self):
+        self.value[0].append("enter")
+        return "enter"
+
+    def __exit__(self, *args):
+        self.value[0].extend(args)
+        return True
+
+    def __mro_entries__(self, bases):
+        return (int, _Dummy)
---
+++ tests/_scopedvar/test_varobject_proxy.py
@@ -0,0 +1,259 @@
+import pytest
+import operator
+from _scopedvar import S, V
+
+from _dummy import _Dummy
+
+tenary_ops = [
+    pow,
+    operator.setitem,
+]
+
+
+@pytest.mark.parametrize('op', tenary_ops)
+def test_tenary_ops(op):
+    a = _Dummy(1)
+    b = _Dummy(2)
+    c = _Dummy(3)
+    vara = S._varfor(V.a['w'])
+    varb = S._varfor(V.b['w'])
+    varc = S._varfor(V.c['w'])
+    with S.isolate():
+        S.assign(vara, a)
+        S.assign(varb, b)
+        S.assign(varc, c)
+        assert op(a, b, c) == op(vara, varb, varc)
+        assert op(a, b, c) == op(vara, varb, c)
+        assert op(a, b, c) == op(vara, b, varc)
+        assert op(a, b, c) == op(vara, b, c)
+
+
+binary_ops = [
+    operator.getitem,
+    operator.delitem,
+    operator.add,
+    operator.sub,
+    operator.mul,
+    operator.mod,
+    operator.pow,
+    operator.lshift,
+    operator.rshift,
+    operator.and_,
+    operator.xor,
+    operator.or_,
+    operator.floordiv,
+    operator.truediv,
+    divmod,
+    operator.matmul,
+    operator.ne,
+    operator.eq,
+    operator.le,
+    operator.lt,
+    operator.ge,
+    operator.gt,
+]
+
+
+@pytest.mark.parametrize('op', binary_ops)
+def test_binary_ops(op):
+    a = _Dummy(1)
+    b = _Dummy(2)
+    vara = S._varfor(V.a['w'])
+    varb = S._varfor(V.b['w'])
+    with S.isolate():
+        S.assign(vara, a)
+        S.assign(varb, b)
+        assert op(a, b) == op(vara, varb)
+        assert op(a, b) == op(a, varb)
+        assert op(a, b) == op(vara, b)
+
+
+unary_ops = [
+    operator.neg,
+    operator.pos,
+    operator.abs,
+    operator.inv,
+    operator.index,
+    reversed,
+    round,
+    (bool, False),
+    (int, 42),
+    (float, 42.1),
+    (complex, 1 + 42j),
+    (bytes, b"test"),
+    (str, "test"),
+]
+
+
+@pytest.mark.parametrize('op', unary_ops)
+def test_unary_ops(op):
+    if isinstance(op, tuple):
+        op, default = op
+    else:
+        default = 1
+    a = _Dummy(default)
+    vara = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(vara, a)
+        assert op(a) == op(vara)
+
+
+inplace_binary_ops = [
+    operator.iadd,
+    operator.isub,
+    operator.imul,
+    operator.imod,
+    operator.ifloordiv,
+    operator.itruediv,
+    operator.ixor,
+    operator.ior,
+    operator.iand,
+    operator.ipow,
+    operator.ilshift,
+    operator.irshift,
+    operator.imatmul,
+]
+
+
+@pytest.mark.parametrize('op', inplace_binary_ops)
+def test_inplace_binary_ops_immutable(op):
+    a = _Dummy(1)
+    b = _Dummy(2)
+    a.immutable = True
+    vara = S._varfor(V.a['w'])
+    varb = S._varfor(V.b['w'])
+    with S.isolate():
+        S.assign(vara, _Dummy(1))
+        S.assign(varb, _Dummy(2))
+        assert op(a, b) == op(vara, varb) is vara
+        assert op(a, b) == op(a, varb)
+        assert op(a, b) == vara
+    assert a == _Dummy(1)
+    assert b == _Dummy(2)
+
+
+@pytest.mark.parametrize('op', inplace_binary_ops)
+def test_inplace_binary_ops_mutable(op):
+    a = _Dummy(1)
+    b = _Dummy(2)
+    a.immutable = False
+    vara = S._varfor(V.a['w'])
+    varb = S._varfor(V.b['w'])
+    with S.isolate():
+        S.assign(vara, _Dummy(1))
+        vara.immutable = False
+        S.assign(varb, _Dummy(2))
+        assert op(a, b) == op(vara, varb) is vara
+        assert vara != _Dummy(1)
+        assert varb == _Dummy(2)
+
+    assert a != _Dummy(1)
+    assert b == _Dummy(2)
+
+
+def test_docstring():
+    class _SubDummy(_Dummy):
+        "sub dummy"
+
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _SubDummy)
+        assert _SubDummy.__doc__ == var.__doc__
+        var.__doc__ = new_doc = "sub dummy 2"
+        assert _SubDummy.__doc__ == new_doc
+
+
+def test_qualname():
+    class _SubDummy(_Dummy):
+        ...
+
+    target_name = "test_qualname.<locals>._SubDummy"
+
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _SubDummy)
+        assert var.__qualname__ == target_name
+        var.__qualname__ = new_name = "new_name"
+        assert _SubDummy.__qualname__ == new_name
+
+
+def test_name():
+    import decimal
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, decimal)
+        assert var.__name__ == "decimal"
+        var.__name__ = new_name = "decimal2"
+        assert decimal.__name__ == new_name
+
+
+def test_module():
+    import sys
+
+    class _SubDummy(_Dummy):
+        ...
+
+    target_module = _SubDummy.__module__
+
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _SubDummy)
+        assert var.__module__ is target_module
+        var.__module__ = sys
+        assert _SubDummy.__module__ is sys
+
+
+def test_class():
+    class _SubDummy(_Dummy):
+        ...
+
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _SubDummy)
+        assert var.__class__ is type
+
+
+def test_annotations():
+    def _f(a: int) -> float:
+        ...
+
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _f)
+        assert var.__annotations__ is _f.__annotations__
+        var.__annotations__ = {'a': str}
+        assert _f.__annotations__ == {'a': str}
+
+
+def test_contextmanager():
+    lst = []
+    obj = _Dummy(lst)
+
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, obj)
+        with var:
+            assert lst == ["enter"]
+        assert lst == ["enter", None, None, None]
+        del lst[:]
+        with var:
+            raise RuntimeError
+        assert lst[1] is RuntimeError
+
+
+def test_mro_entries():
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _Dummy(0))
+
+        class _SubDummy(var):
+            ...
+
+        assert _SubDummy.__bases__ == (int, _Dummy)
+
+
+def test_call():
+    var = S._varfor(V.a['w'])
+    with S.isolate():
+        S.assign(var, _Dummy)
+        assert var(0) == _Dummy(0)
